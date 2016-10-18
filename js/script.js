2a66acefecaded7912acf2f0181db5dd04eb9732
ksenialass@gmail.com <Lass@MacBook-Air-Ksenia.local>
2016-10-18 15:42:46 +0300

    add animation

--- js/script.js
+++ js/script.js
@@ -1,52 +1,89 @@
 (function( $ ) {
     var defaults = {
         auto: true,
-        speed: 5000
-    }
+        speed: 5000,
+        animation: true
+    };
     $.fn.slider = function(params) {
         params = $.extend({}, defaults, params);
         var self = this,
             cont = self.find('.slider-cont'),
             item = self.find('.slider-item'),
             slideCount = item.length,
-            active = 0,
+            active = cont.find('.slider-item.active').index(), // Поиск активного (первого для показа) слайда
             itemLen = slideCount -1,
             methods = {
                 init: function() {
-                    cont.append('<div class="slide-button next"></div><div class="slide-button prev"></div>');
-                    this.show(active);
+                    //cont.append('<div class="slide-button next"></div><div class="slide-button prev"></div>'); Перенесено в HTML
 
+                    if(active < 0) {
+                        active = 0; // Если активный слайдер не указан, по умолчанию первый
+                    }
+
+                    if(params.animation) {
+                        this.getWidth();
+                        $(self).addClass('animation');
+                        this.animation(active);
+                    } else {
+                        this.showSlide(active);
+                    }
                     if(params.auto) {
                         this.autoplay();
                     }
+
+                    self.addClass('slider-active');
                 },
-                show: function(active) {
+                showSlide: function(active) {
                     cont.find('.slider-item').eq(active).addClass('active').siblings().removeClass('active');
                 },
                 next: function() {
                     if(active == itemLen) {
                         active = 0;
-                        return this.show(0);
+                        if(params.animation) {
+                            return this.animation(0)
+                        } else {
+                            return this.showSlide(0);
+                        }
                     } else {
                         active++;
-                        return this.show(active);
+                        if(params.animation) {
+                            return this.animation(active);
+                        } else {
+                            return this.showSlide(active);
+                        }
                     }
                 },
                 prev: function() {
                     if(active == 0) {
                         active = itemLen;
-                        return this.show(itemLen);
+                        if(params.animation) {
+                            return this.animation(itemLen);
+                        } else {
+                            return this.showSlide(itemLen);
+                        }
                     } else {
                         active--;
-                        return this.show(active);
+                        if(params.animation) {
+                            return this.animation(active);
+                        } else {
+                            return this.showSlide(active);
+                        }
                     }
                 },
                 autoplay: function() {
                     $(self).hover(
                         function(){clearInterval(timer);},
-                        function(){timer = setInterval(function () {methods.next();}, (params.speed));}
+                        function(){timer = setInterval(function () {methods.next();}, params.speed);}
                     );
-                    var timer = setInterval(function () {methods.next();}, (params.speed));
+                    var timer = setInterval(function () {methods.next();}, params.speed);
+                },
+                getWidth: function() {
+                    item.css('width', 100 / slideCount + "%");
+                    cont.css('width', 100 * slideCount + "%");
+                },
+                animation: function(active) {
+                    var position = -($(window).width() * active);
+                    cont.css('transform', 'translate('+position+'px)');
                 }
             };
         methods.init();
@@ -58,13 +95,18 @@
                 methods.prev();
             }
         });
+        $(window).resize(function(){
+            methods.getWidth();
+            methods.animation(active);
+        })
     };
 })(jQuery);
 
-$('.slider').slider({
-    auto: true,
-    speed: 2000
-});
+$('.slider').slider();
+
 $('.slider2').slider({
-    auto: false
+    auto: false,
+    speed: 4000,
+    animation: false
 });
+$('.slider-text').slider();
